e45f64c0c117af1219a81d967837fda71601138d
Artem Korchunov <artem.korchunof@gmail.com>
2019-01-06 13:28:13 +0200

    work around with apollo consumer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, ApolloConsumer } from "react-apollo";
+import gql from "graphql-tag";
+
+import "./App.css";
+
+import Content from "./Content";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/2000px-GraphQL_Logo.svg.png"
+            }
+            className="App-logo"
+            alt="logo"
+          />
+          <p>GraphQL</p>
         </header>
+        <ApolloProvider client={client}>
+          <ApolloConsumer>
+            {client => {
+              client
+                .query({
+                  query: gql`
+                    {
+                      recipes {
+                        id
+                        title
+                      }
+                    }
+                  `
+                })
+                .then(result => console.log(result));
+              return <Content />;
+            }}
+          </ApolloConsumer>
+        </ApolloProvider>
       </div>
     );
   }
